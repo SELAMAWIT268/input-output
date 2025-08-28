ea40a8cc143975a1db324ab4c358ffebbda2dc42
SELAMAWIT268 <gebreselina711@gmail.com>
2025-08-28 03:13:30 -0700

    developed by selina

--- string.py
+++ string.py
@@ -1,1 +1,47 @@
+s = "hello"
+print(s[::-1])
+
+
+s = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+count = sum(1 for char in s if char in vowels)
+print("Number of vowels:", count)
+
+
+def are_anagrams(s1, s2):
+    return sorted(s1) == sorted(s2)
+
+print(are_anagrams("listen", "silent"))
+
+
+sentence = "this is a test this is"
+words = sentence.split()
+count = {}
+for w in words:
+    count[w] = count.get(w, 0) + 1
+print(count)
+
+
+def caesar_cipher(text, shift):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            base = ord('A') if char.isupper() else ord('a')
+            result += chr((ord(char) - base + shift) % 26 + base)
+        else:
+            result += char
+    return result
+
+print(caesar_cipher("hello", 3))  
+
+
+import re
+
+email = input("Enter email: ")
+pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+if re.match(pattern, email):
+    print("Valid email")
+else:
+    print("Invalid email")
 
